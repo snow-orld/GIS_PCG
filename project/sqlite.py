2afc5b674730de6a1dedf8b38f3f13222b6e05f0
snow-orld <mou.xueman@gmail.com>
2017-11-02 17:01:08 +0800

    add preparse for sqlite insertion

--- project/sqlite.py
+++ project/sqlite.py
@@ -6,36 +6,52 @@ Synthesize .shp related files in a folder to one single OpenDRIVE formatted .XML
 author: Xueman Mou
 date: 2017/10/30
 version: 1.0.0
-modified: 2017/10/30 15:50:00 GMT+800
+modified: 2017/11/1 14:07:00 GMT+800
 
 developing env: python 3.6.2
 dependencies  :	sqlite3
 
-input :	path to EMG sample data folder
+input :	path to EMG sample data folder (by calling preparse.py)
 output: single OpenDRIVE-like .XML file
 """
 
 import sqlite3
 
-conn = sqlite3.connect('example.db')
-c = conn.cursor()
+# DB_NAME = 'EMG.db'
+DB_NAME = 'tutorial.db'
 
-# Create table
-c.execute('''CREATE TABLE stocks
-             (date text, trans text, symbol text, qty real, price real)''')
+# Class DataBase
+class DataBase(object):
+	
+	def __init__(self, name):
+		self.db_name = name
+		self.c = sqlite3.connect(self.db_name).cursor()
+		self.tables = {}	# key - table name, value - schema ?
 
-# Insert a row of data
-c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+	def __repr__(self):
+		return '{} has {} tables'.format(self.db_name, len(self.tables))
 
-# Save (commit) the changes
-conn.commit()
+	def insert(self, table, tuples):
+		print('inserting ...')
 
-# We can also close the connection if we are done with it.
-# Just be sure any changes have been committed or they will be lost.
-conn.close()
+	def delete(self):
+		print('deleting ...')
+
+	def search(self):
+		print('searching ...')
+
+	def update(self):
+		print('updating ...')
 
 def main():
-	pass
+	db = DataBase(DB_NAME)
+	test(db)
+
+# Test
+def test(db):
+	# Create table
+	db.c.execute('''CREATE TABLE stocks
+					(date text, trans text, symbol text, qty real, price real)''')
 
 if __name__ == '__main__':
 	main()
